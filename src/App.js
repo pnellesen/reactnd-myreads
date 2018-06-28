5ccf7690217c8bb516bf3e28aa44a30197303e2d
Pat Nellesen <pnellesen@hotmail.com>
2018-06-28 05:42:25 +0000

    Added some comments for setting up basic starter component structure

--- src/App.js
+++ src/App.js
@@ -44,15 +44,30 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
+          	 	{/* 
+                	Create a "Bookshelf" component, takes a "shelfId" prop of one the following: {'currentlyReading', 'wantToRead', 'read'}. Might also take a "title" prop.
+                    componentDidMount will retrieve all books on the shelf specified by the shelfId prop. 
+                    State might be something like {id: shelfId, booklist: [array of current book ids]}
+                */}
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
+          				{/*
+                        	Create a "Book" component, takes prop of bookId. Used to get the various attributes of the book from BooksAPI.js.
+                            State will likely be something like "currentShelf". Probably want this state to be controlled by the book component.
+                        */}
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
+                            
+          					{/*
+                            	Create a "BookshelfChanger" component. Takes a prop called "bookInfo", an object of the form {bookId: id, currentShelf: currentShelf}. used to determine which
+                                option is selected. onSelect will move the book to the correct bookshelf via BooksAPI.js
+                                
+                            */}
+          					<div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
